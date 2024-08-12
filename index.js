990e4a6c1426001ab7d4c14774b384c75a6e4524
Habibur Rahman <habiburkuetacbd@gmail.com>
2024-08-12 19:44:24 +0600

    worked on post, get and put method

--- index.js
+++ index.js
@@ -12,12 +12,26 @@ app.get('/', (req, res) => {
 
 app.get('/api/todos', async (req, res) => {
     try{
-        const todos = await Todo.find();
+        const todos = await Todo.find({});
         res.status(200).json(todos);
     }catch(err){
-        res.status(500).json(err.message);
+        res.status(500).json({message: err.messsage});
     }
 
+});
+
+app.get('/api/todos/:id', async (req, res) => {
+        const {id} = req.params;
+        try{
+            const todo = await Todo.findById(id);
+            if(!todo){
+                return res.status(404).json({message: 'Todo not found'});
+            }
+            res.status(200).json(todo);
+        }catch(err){
+            console.log(err);
+            res.status(500).json({message: err.messsage});
+        }
 });
 
 app.post('/api/addTodo', async(req, res) => {
@@ -26,9 +40,24 @@ app.post('/api/addTodo', async(req, res) => {
         res.status(201).json(todo);
     }catch(err){
         console.log(err.messsage);
-        res.status(500).json(err.messsage);
+        res.status(500).json({message: err.messsage});
     }
 });
+
+app.put('/api/updateTodo/:id', async(req, res) => {
+    const {id} = req.params;
+    try{
+        const updateTodo = await Todo.findByIdAndUpdate(id, req.body);
+        if(!updateTodo){
+            return res.status(404).json({message: 'Todo not found'});
+        }
+        res.status(200).json(updateTodo);
+    }catch(err){
+        res.status(500).json({message: err.messsage});
+    }
+
+});
+
 
 
 // Connect to MongoDB
