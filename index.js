4befe8228c6fedc586179933e1910fff32f28471
Habibur Rahman <habiburkuetacbd@gmail.com>
2024-08-13 07:24:28 +0600

    completed crud in simple way

--- index.js
+++ index.js
@@ -55,7 +55,20 @@ app.put('/api/updateTodo/:id', async(req, res) => {
     }catch(err){
         res.status(500).json({message: err.messsage});
     }
+});
 
+app.delete('/api/deleteTodo/:id', async (req, res) => {
+    const {id} = req.params;
+
+    try{
+        const todo = await Todo.findByIdAndDelete(id);
+        if(!todo){
+            return res.status(404).json({message: 'Todo not found'});
+        }
+        res.status(200).json({message: 'Todo deleted'});
+    }catch(err){
+        res.status(500).json({message: err.messsage});
+    }
 });
 
 
